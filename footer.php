60ae9d57bae6b0ab7ea5bf63d0a9fe550e0952b4
Md Mijanur Molla <78760580+TheMijanur-9292@users.noreply.github.com>
2025-01-21 09:34:50 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="">Terms & Conditions</a>
+         <a href="">Privacy Policy</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Info</h3>
+         <p> <i class="fas fa-phone"></i> +91 8327362533 </p>
+         <!--<p> <i class="fas fa-phone"></i>  </p> -->
+         <p> <i class="fas fa-envelope"></i>advineestore@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i>Ecospace,Kolkata,700156 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/advine.ecom.academy?mibextid=ZbWKwL" target="_blank"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://youtube.com/@advineecomacademy?si=aPza-bFYSbVcm4LK" target="_blank"> <i class="fab fa-youtube"></i> Youtube </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/company/advine-consultancy-services/" target="_blank"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Copyright  @ <?php echo date('Y'); ?> by <span href="" target="_blank">MNB</span> </p>
+
+</section>
